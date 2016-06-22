c823845787ccc33d785578003b1e03ce14ea9ded
Puneet Sahalot <puneetsahalot@gmail.com>
2016-06-23 01:04:28 +0530

    added external links for BB

--- bb-helper.php
+++ bb-helper.php
@@ -50,7 +50,7 @@
           */
          public function __construct() {
              // register actions
-             add_action('wp_before_admin_bar_render', array(&$this, 'ps_bb_helper_admin_bar_render'), 100);
+             add_action('admin_bar_menu', array(&$this, 'ps_bb_helper_menu'), 98);
          } // END public function __construct
 
          /**
@@ -71,49 +71,153 @@
          /**
           * Adds admin bar items for easy access to the theme creator and editor
           */
-         public function ps_bb_helper_admin_bar_render() {
-             $this->ps_bb_helper_admin_bar_render_item('Beaver Helepr'); // Parent item
-             $this->ps_bb_helper_admin_bar_render_item('Clear Cache', 'some_link_to_the_settings', 'Beaver Helepr');
-             $this->ps_bb_helper_admin_bar_render_item('BAW Sub2', 'some_link_to_the_settings', 'Beaver Helepr');
-         }
-
-         /**
-          * Adds menu parent or submenu item.
-          * @param string $name the label of the menu item
-          * @param string $href the link to the item (settings page or ext site)
-          * @param string $parent Parent label (if creating a submenu item)
-          *
-          * @return void
-          * */
-         public function ps_bb_helper_admin_bar_render_item( $name, $href = '', $parent = '', $custom_meta = array() ) {
-             global $wp_admin_bar;
-
-         	if ( ! is_super_admin()
-         		 || ! is_object( $wp_admin_bar )
-         		 || ! function_exists( 'is_admin_bar_showing' )
-         		 || ! is_admin_bar_showing() ) {
+         public function ps_bb_helper_menu() {
+             /**
+         	 * Required WordPress cabability to display new toolbar / admin bar entry
+         	 * Only showing items if toolbar / admin bar is activated and user is logged in!
+         	 *
+         	 * @since 1.0.0
+         	 */
+         	if ( ! is_user_logged_in() || ! is_admin_bar_showing() ) {
          		return;
          	}
 
-             // Generate ID based on the current filename and the name supplied.
-             $id = sanitize_key( basename(__FILE__, '.php' ) . '-' . $name );
+            /** Show these items only if Beaver Builder plugin is actually installed */
+	        if ( ( function_exists( 'is_plugin_active' ) && ( is_plugin_active( 'beaver-builder-lite-version/fl-builder.php' ) || is_plugin_active ('bb-plugin/fl-builder.php') ) ) ) {
+                global $wp_admin_bar;
+                // set BB active variable for later use.
+                $bb_active = 'bb_is_active';
 
-             // Generate the ID of the parent.
-             $parent = sanitize_key( basename(__FILE__, '.php' ) . '-' . $parent );
+                if ( current_user_can('manage_options') ) {
 
-             // links from the current host will open in the current window
+                        // Add top level link
+                        $args = array(
+                    		'id'          => 'beaver-helper',
+                    		'title'       => __('Beaver Helper', 'ps-bb-helper'),
+                    		'href'        => '#'
+                    	);
+                    	$wp_admin_bar->add_node($args);
 
-             $meta = strpos( $href, site_url() ) !== false ? array() : array( 'target' => '_blank' ); // external links open in new tab/window
-             $meta = array_merge( $meta, $custom_meta );
+                        // Add first child link
+                        $args = array(
+                            'id'        => 'beaver-facebook-group',
+                            'title'     => __('Facebook Group', 'ps-bb-helper'),
+                            'href'      => 'https://www.facebook.com/groups/beaverbuilders/',
+                            'parent'    => 'beaver-helper',
+                            'meta'      => array (
+                                        'class'     => 'beaver-helper-facebook',
+                                        'title'     => 'Visit BB Facebook Group'
+                            )
+                        );
+                        $wp_admin_bar->add_node($args);
 
-             $wp_admin_bar->add_node( array(
-                 'parent' => $parent,
-                 'id' => $id,
-                 'title' => $name,
-                 'href' => $href,
-                 'meta' => $meta,
-             ) );
+                        // Add second child link
+                        $args = array(
+                            'id'        => 'beaver-slack-channel',
+                            'title'     => __('Slack Channel', 'ps-bb-helper'),
+                            'href'      => 'https://beaverbuilders.herokuapp.com/',
+                            'parent'    => 'beaver-helper',
+                            'meta'      => array (
+                                        'class'     => 'beaver-helper-slack',
+                                        'title'     => 'Join BB Slack Channel'
+                            )
+                        );
+                        $wp_admin_bar->add_node($args);
+
+                        // Add third child link
+                        $args = array(
+                            'id'        => 'beaver-kb',
+                            'title'     => __('Knowledge Base', 'ps-bb-helper'),
+                            'href'      => 'https://www.wpbeaverbuilder.com/knowledge-base/',
+                            'parent'    => 'beaver-helper',
+                            'meta'      => array (
+                                        'class'     => 'beaver-kb',
+                                        'title'     => 'Visit Knowledge Base'
+                            )
+                        );
+                        $wp_admin_bar->add_node($args);
+
+                        // Add fourth child link
+                        $args = array(
+                            'id'        => 'beaver-official-support',
+                            'title'     => __('Official Support', 'ps-bb-helper'),
+                            'href'      => 'https://www.wpbeaverbuilder.com/beaver-builder-support/',
+                            'parent'    => 'beaver-helper',
+                            'meta'      => array (
+                                        'class'     => 'beaver-support',
+                                        'title'     => 'Sumbit a Support Ticket'
+                            )
+                        );
+                        $wp_admin_bar->add_node($args);
+                }
+            }
+
+
          }
+
+
+
+         /*
+            * add a group of links under a parent link
+            */
+
+            // Add a parent shortcut link
+
+            public function custom_toolbar_link($wp_admin_bar) {
+            	$args = array(
+            		'id' => 'wpbeginner',
+            		'title' => 'WPBeginner',
+            		'href' => 'https://www.wpbeginner.com',
+            		'meta' => array(
+            			'class' => 'wpbeginner',
+            			'title' => 'Visit WPBeginner'
+            			)
+            	);
+            	$wp_admin_bar->add_node($args);
+
+            // Add the first child link
+
+            	$args = array(
+            		'id' => 'wpbeginner-guides',
+            		'title' => 'WPBeginner Guides',
+            		'href' => 'http://www.wpbeginner.com/category/beginners-guide/',
+            		'parent' => 'wpbeginner',
+            		'meta' => array(
+            			'class' => 'wpbeginner-guides',
+            			'title' => 'Visit WordPress Beginner Guides'
+            			)
+            	);
+            	$wp_admin_bar->add_node($args);
+
+            // Add another child link
+            $args = array(
+            		'id' => 'wpbeginner-tutorials',
+            		'title' => 'WPBeginner Tutorials',
+            		'href' => 'http://www.wpbeginner.com/category/wp-tutorials/',
+            		'parent' => 'wpbeginner',
+            		'meta' => array(
+            			'class' => 'wpbeginner-tutorials',
+            			'title' => 'Visit WPBeginner Tutorials'
+            			)
+            	);
+            	$wp_admin_bar->add_node($args);
+
+            // Add a child link to the child link
+
+            $args = array(
+            		'id' => 'wpbeginner-themes',
+            		'title' => 'WPBeginner Themes',
+            		'href' => 'http://www.wpbeginner.com/category/wp-themes/',
+            		'parent' => 'wpbeginner-tutorials',
+            		'meta' => array(
+            			'class' => 'wpbeginner-themes',
+            			'title' => 'Visit WordPress Themes Tutorials on WPBeginner'
+            			)
+            	);
+            	$wp_admin_bar->add_node($args);
+
+            }
+
 
      } // END class PS_Beaver_Helper
  } // END if(!class_exists('PS_Beaver_Helper'))
